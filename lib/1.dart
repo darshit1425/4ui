c9ca8a95e8c8e4cdd78d23fc94d76289fe29dc62
r51 <darshitmendapara1425@gmail.com>
2023-01-16 07:05:10 -0800

    2

--- lib/1.dart
+++ lib/1.dart
@@ -8,7 +8,8 @@ class screen1 extends StatefulWidget {
 }
 
 class _screen1State extends State<screen1> {
-Color c1=Color(0xff8F8F8F);
+  Color c1 = Color(0xff8F8F8F);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -49,17 +50,406 @@ Color c1=Color(0xff8F8F8F);
                     ],
                   ),
                 ),
-                // SizedBox(
-                //   height: 10,
-                // ),
+                SizedBox(
+                  height: 10,
+                ),
                 Divider(
                   height: 2,
                   color: Color(0xff8F8F8F),
                   thickness: 0.3,
                 ),
                 Container(
+                  height: 40,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.add_circle,
+                          color: Colors.blueAccent.shade100,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "About Phone",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          width: 150,
+                        ),
+                      ),
+                      Text(
+                        "MIUI 10 Global 9.2.28",
+                        style: TextStyle(color: Color(0xff8F8F8F)),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                        color: Color(0xff8F8F8F),
+                      )
+                    ],
+                  ),
+                ),
+                Divider(
+                  height: 2,
+                  color: Color(0xff8F8F8F),
+                  thickness: 0.3,
+                ),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.system_security_update_good_outlined,
+                          color: Colors.blueAccent.shade100,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "System apps updater",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          width: 150,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Color(0xff8F8F8F),
+                      )
+                    ],
+                  ),
+                ),
+                Divider(
+                  height: 25,
+                  color: Colors.grey.shade200,
+                  thickness: 8,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "WIRELESS & NETWORKS",
+                      style: TextStyle(fontSize: 11),
+                    ),
+                  ],
+                ),
+                Divider(
+                  height: 25,
+                  color: Colors.grey.shade200,
+                  thickness: 2,
+                ),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.sim_card_alert_outlined,
+                          color: Colors.blueAccent.shade100,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "SIM cards & mobile networks",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          width: 150,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                        color: Color(0xff8F8F8F),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  height: 2,
+                  color: Colors.grey.shade200,
+                  thickness: 0.9,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.wifi,
+                          color: Colors.blueAccent.shade100,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "Wi-Fi",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          width: 150,
+                        ),
+                      ),
+                      Text("Off"),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                        color: Color(0xff8F8F8F),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  height: 2,
+                  color: Colors.grey.shade200,
+                  thickness: 0.9,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.bluetooth,
+                          color: Colors.purple.shade200,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "Bluetooth",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          width: 150,
+                        ),
+                      ),
+                      Text("Off"),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                        color: Color(0xff8F8F8F),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Divider(
+                  height: 2,
+                  color: Colors.grey.shade200,
+                  thickness: 0.9,
+                ),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.podcasts_rounded,
+                          color: Colors.deepOrange.shade200
+                          ,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "Portable hotspot",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          width: 150,
+                        ),
+                      ),
+                      Text("Off"),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                        color: Color(0xff8F8F8F),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Divider(
+                  height: 2,
+                  color: Colors.grey.shade200,
+                  thickness: 0.9,
+                ),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.vpn_key,
+                          color: Colors.grey.shade600,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "VPN",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          width: 150,
+                        ),
+                      ),
+                      Text("Off"),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                        color: Color(0xff8F8F8F),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Divider(
+                  height: 2,
+                  color: Colors.grey.shade200,
+                  thickness: 0.9,
+                ),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.data_usage,
 
+                          color: Colors.blue.shade300
+                          ,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "Data usage",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          width: 150,
+                        ),
+                      ),
 
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                        color: Color(0xff8F8F8F),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Divider(
+                  height: 2,
+                  color: Colors.grey.shade200,
+                  thickness: 0.9,
+                ),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.more_horiz,
+
+                          color: Colors.blue.shade200
+
+                          ,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "More",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          width: 150,
+                        ),
+                      ),
+
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20,
+                        color: Color(0xff8F8F8F),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  //height: 2,
+                  color: Colors.grey.shade200,
+                  thickness: 10,
+                ),
+                Row(
+                  children: [
+                    Text("PERSONAL",style: TextStyle(fontSize: 12,color: Colors.grey.shade500),),
+                  ],
                 ),
               ],
             ),
